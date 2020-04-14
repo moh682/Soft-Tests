11a1bc7f7338d6c2a968cff8e958f708e91827cd
Rasmus Jarnborg Friis <rasmusjf@hotmail.com>
2020-04-14 14:31:25 +0200

    Added movement route

--- assignments/Assignment4/src/routes/MovementRoute.ts
+++ assignments/Assignment4/src/routes/MovementRoute.ts
@@ -1,7 +1,44 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import { MovementMapper } from '../database/MovementMapper';
+import { IMovement } from '../interfaces/IMovement';
 const route = express();
 
-route.post('/login', async (req: Request, res: Response) => {});
 
-route.post('/register', async (req: Request, res: Response) => {});
+const mm = new MovementMapper();
+
+route.post('/movement/create', async (req, res, next) => {
+    const movement: IMovement = {
+        id: req.body.id,
+        time: req.body.time,
+        amount: req.body.amount,
+        accountFrom: req.body.accountFrom,
+        accountTo: req.body.accountTo,
+    };
+    mm.insert(movement);
+    if (Error) {
+        res.status(400);
+    } else {
+        res.status(200)
+    }
+});
+
+route.delete('/movement/delete', async (req, res, next) => {
+    mm.deleteById(req.body.id);
+    if (Error) {
+        res.status(400);
+    } else {
+        res.status(200)
+    }
+});
+
+route.get('/customer/find', async (req, res, next) => {
+    const movement = mm.getById(req.body.id);
+    if (Error) {
+        res.status(400);
+    } else {
+        res.json(movement)
+    }
+});
+
 export { route as MovementRoute };
+
