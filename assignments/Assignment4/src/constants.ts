e58265807b883b79207ed1cc2b62a38ed68d857f
Mohammad Omar Salah Hariri <mosh@proactive.dk>
2020-06-14 12:58:56 +0200

    fix constants

--- assignments/Assignment4/src/constants.ts
+++ assignments/Assignment4/src/constants.ts
@@ -1,12 +1,15 @@
 const env = require('dotenv').config().parsed;
 
-export const MYSQL_USERNAME = env?.MYSQL_USERNAME || process.env.MYSQL_USERNAME;
-export const MYSQL_PASSWORD = env?.MYSQL_PASSWORD || process.env.MYSQL_PASSWORD;
+let MYSQL_USERNAME = env?.MYSQL_USERNAME || process.env.MYSQL_USERNAME;
+let MYSQL_PASSWORD = env?.MYSQL_PASSWORD || process.env.MYSQL_PASSWORD;
 let SERVER_PORT = env?.SERVER_PORT || process.env.SERVER_PORT;
-export const MYSQL_DATABASE = env?.MYSQL_DATABASE || process.env.MYSQL_DATABASE;
+let MYSQL_DATABASE = env?.MYSQL_DATABASE || process.env.MYSQL_DATABASE;
 
 if (process.env.ENV === 'TEST') {
-  SERVER_PORT = 4000;
+	SERVER_PORT = 4000;
+	MYSQL_USERNAME = process.env.TEST_MYSQL_USERNAME;
+	MYSQL_PASSWORD = process.env.TEST_MYSQL_PASSWORD;
+	MYSQL_DATABASE = process.env.DATABASE
 }
 
-export { SERVER_PORT };
+export { SERVER_PORT, MYSQL_PASSWORD, MYSQL_USERNAME, MYSQL_DATABASE };
